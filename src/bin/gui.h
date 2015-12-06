f178802f0b970330456e87975577a20948cc5eaf
Daniel Zaoui <daniel.zaoui@samsung.com>
2015-12-06 14:10:11 +0200

    Revert "Adding screenshot support"
    This reverts commit a517c9a52220f5e771936f8dafc28107a08eb829.
    
    Screenshot feature has not been added in debug repository yet.

--- src/bin/gui.h
+++ src/bin/gui.h
@@ -40,13 +40,6 @@ typedef struct
 } Gui_Screenshot_Button_Widgets;
 
 
-typedef struct
-{
-   Eo *screenshot_win;
-   Eo *img;
-} Gui_Screenshot_Win_Widgets;
-
-
 typedef struct {
      Gui_Main_Win_Widgets *main_win;
 } Gui_Widgets;
@@ -58,8 +51,6 @@ Gui_Profiles_Win_Widgets *gui_profiles_win_create(Eo *parent);
 Gui_New_Profile_Win_Widgets *gui_new_profile_win_create(Eo *parent);
 
 Gui_Screenshot_Button_Widgets *gui_screenshot_button_create(Eo *parent);
-
-Gui_Screenshot_Win_Widgets *gui_screenshot_win_create(Eo *parent);
 
 Gui_Widgets *gui_gui_get();
 #endif
