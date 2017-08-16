dc7ee20d2211951bd91f9a11ac5990744aaf5cec
Daniel Zaoui <daniel.zaoui@yahoo.com>
2017-08-16 20:42:26 +0300

    Reorder includes to get the EAPI definition

--- src/lib/extensions/objects_introspection/main.c
+++ src/lib/extensions/objects_introspection/main.c
@@ -3,8 +3,8 @@
 
 #include "../../Clouseau_Debug.h"
 
-#include "../../Clouseau.h"
 #include "gui.h"
+#include "../../Clouseau.h"
 
 #define _EET_ENTRY "config"
 
@@ -25,32 +25,6 @@
 #define SWAP_16(x) eina_swap16(x)
 #define SWAP_DBL(x) SWAP_64(x)
 #endif
-
-#ifdef EAPI
-# undef EAPI
-#endif
-
-#ifdef _WIN32
-# ifdef EFL_EO_BUILD
-#  ifdef DLL_EXPORT
-#   define EAPI __declspec(dllexport)
-#  else
-#   define EAPI
-#  endif /* ! DLL_EXPORT */
-# else
-#  define EAPI __declspec(dllimport)
-# endif /* ! EFL_EO_BUILD */
-#else
-# ifdef __GNUC__
-#  if __GNUC__ >= 4
-#   define EAPI __attribute__ ((visibility("default")))
-#  else
-#   define EAPI
-#  endif
-# else
-#  define EAPI
-# endif
-#endif /* ! _WIN32 */
 
 static int _eoids_get_op = EINA_DEBUG_OPCODE_INVALID;
 static int _klids_get_op = EINA_DEBUG_OPCODE_INVALID;
